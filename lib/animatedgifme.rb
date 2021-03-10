376143dfb6be4fbd761ebfc83618d7e14401a9cc
Kurnia Muhamad <omenkzz@gmail.com>
2021-03-10 12:04:23 +0700

    Initial project

---
+++ lib/animatedgifme.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "animatedgifme/version"
+
+module Animatedgifme
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/animatedgifme/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Animatedgifme
+  VERSION = "0.1.0"
+end
